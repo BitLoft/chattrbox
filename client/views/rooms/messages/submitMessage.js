b9382d560e3d1a6f156adc68c5cd81de09d556a6
Tj Houston <tj@tjhouston.com>
2013-09-23 21:04:32 -0400

    remove gany and added twitter profile name

--- client/views/rooms/messages/submitMessage.js
+++ client/views/rooms/messages/submitMessage.js
@@ -4,6 +4,8 @@ Template.submitMessage.events({
         var message = {
             timeStamp: new Date(Date())/1,
             sender:Meteor.user().username,
+            sender:Meteor.user().profile.name,
+
             message:$(e.target).find('[name=message]').val(),
             roomUID:Session.get('roomUID')
         }
